3f831a2e33237207fbbd86b7bf9e43a7076f835f
davidkatz <15Dkatz@shcp.edu>
2017-04-11 14:36:36 -0700

    better Wheel example

--- chapter2/Gear.rb
+++ chapter2/Gear.rb
@@ -1,10 +1,9 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog       = cog
-    @rim       = rim
-    @tire      = tire
+    @wheel     = wheel
   end
 
   def ratio
@@ -12,9 +11,30 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
   end
-
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim  = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumfrence
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumfrence
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
